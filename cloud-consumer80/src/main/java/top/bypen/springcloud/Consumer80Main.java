58b393fe56c8e7c9bb208df7d9640e94b01b364c
liangjinpeng <2209085713@qq.com>
2020-06-15 15:11:13 +0800

    cloud-consumer80

---
+++ cloud-consumer80/src/main/java/top/bypen/springcloud/Consumer80Main.java
@@ -0,0 +1,16 @@
+package top.bypen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: LiangJinpeng
+ * @Date: 2020/6/15 14:26
+ */
+@SpringBootApplication
+public class Consumer80Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer80Main.class, args);
+    }
+}
