f681865a7b7b3146a20f4897982dee002006a0f0
liangjinpeng <2209085713@qq.com>
2020-06-15 17:36:44 +0800

    Eureka注册中心

--- cloud-eureka-server7001/src/main/java/top/bypen/springcloud/EurekaServer7001Main.java
+++ cloud-eureka-server7001/src/main/java/top/bypen/springcloud/EurekaServer7001Main.java
@@ -2,17 +2,16 @@ package top.bypen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author: LiangJinpeng
- * @Date: 2020/6/15 14:26
+ * @Date: 2020/6/15 17:00
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class Consumer80Main {
-
+@EnableEurekaServer
+public class EurekaServer7001Main {
     public static void main(String[] args) {
-        SpringApplication.run(Consumer80Main.class, args);
+        SpringApplication.run(EurekaServer7001Main.class, args);
     }
 }
