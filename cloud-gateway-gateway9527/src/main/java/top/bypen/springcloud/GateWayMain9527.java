f0f1bb522b39f6eff499ae3f83e49cefbd5f8147
liangjinpeng <2209085713@qq.com>
2020-06-23 18:14:21 +0800

    GateWay完成

---
+++ cloud-gateway-gateway9527/src/main/java/top/bypen/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package top.bypen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: LiangJinpeng
+ * @Date: 2020/6/23 11:51
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
